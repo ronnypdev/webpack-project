daea0a9e641c570a29d0d5d53d64950e463d84d0
Ronny Perez <ronnypdev@gmail.com>
2022-05-01 17:57:37 -0400

    added babel

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4
 }
 
+const elvenGuantLetsRecepie = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4
+}
+
 console.log(elvenShieldRecipe);
+console.log(elvenGuantLetsRecepie);
